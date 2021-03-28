51fe9d5fab89d59eee3139aa6de5a23b3be8b902
김동환 <1033513@naver.com>
2021-03-28 13:35:29 +0900

    Initial Commit

---
+++ CollectionViewPractice/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CollectionViewPractice
+//
+//  Created by 김동환 on 2021/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1000
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
+        
+        cell.backgroundColor = UIColor(red: CGFloat(arc4random_uniform(255))/255.0, green: CGFloat(arc4random_uniform(255))/255.0, blue: CGFloat(arc4random_uniform(255))/255.0, alpha: 1.0)
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("\(indexPath.row)")
+    }
+}
